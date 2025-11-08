85adf9b550ed527da112cf61ebc6b906a7897032
Jadilson <jadilsonigor@gmail.com>
2025-11-08 18:20:59 -0300

    feat: adding delete, put and get with query params endpoints

--- src/main/java/com/estudo/projeto_estudo/controller/ProdutoController.java
+++ src/main/java/com/estudo/projeto_estudo/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.estudo.projeto_estudo.model.Produto;
 import com.estudo.projeto_estudo.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -32,5 +33,29 @@ public class ProdutoController {
     public Produto obterPorId(@PathVariable("id") String id) {
         Optional<Produto> produto = produtoRepository.findById(id);
         return produto.isPresent() ? produto.get() : null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Produto excluir(@PathVariable("id") String id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if (produto.isPresent()) {
+            produtoRepository.delete(produto.get());
+        }
+        return produto.isPresent() ? produto.get() : null;
+    }
+
+    @PutMapping("/{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+        Optional<Produto> existingProduto = produtoRepository.findById(id);
+        if (existingProduto.isPresent()) {
+            produto.setId(id);
+            produtoRepository.save(produto);
+        }
+
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
     }
 }
